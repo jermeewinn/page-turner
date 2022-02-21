311fc0d703074e6be7a946666ae1f2d0af03132a
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-20 19:35:36 -0800

    completed typeDefs for server/schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const gql = require('apollo-server-express');
+
+const typeDefs = gql`
+    input customBook {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }    
+
+    type Query {
+            me: User
+        }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(email: String!, password: String!, username: String!): Auth
+        saveBook(book: customBook): User
+        removeBook(): User        
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: STring
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
