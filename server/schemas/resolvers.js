0979f25b4e13f275e644e36825e18851133a889b
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-20 19:45:51 -0800

    implemented preliminary 'me' query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,21 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { saveBook } = require("../controllers/user-controller");
+const { User } = require("../models");
+const { signToken } = require('../utils/auth');
 
 
 const resolvers = {
     Query: {
-
+        // Me query
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({})
+                    .select('-__v -password')
+                    .populate(saveBook);
+                    return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        }
     },
     Mutation: {
 
