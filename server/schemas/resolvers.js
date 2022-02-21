e118b4f38ab8558dac5c2466279221bbeefed9e4
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-20 20:00:58 -0800

    set up initial structure for mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { saveBook } = require("../controllers/user-controller");
+//NOTE TO SELF: will need to get rid of const { saveBook } eventually
 const { User } = require("../models");
 const { signToken } = require('../utils/auth');
 
@@ -11,15 +12,31 @@ const resolvers = {
             if (context.user) {
                 const userData = await User.findOne({})
                     .select('-__v -password')
-                    .populate(saveBook);
+                    .populate('saveBook');
                     return userData;
             }
             throw new AuthenticationError('Not logged in');
         }
     },
     Mutation: {
+        // User login
+        login: async (parent { email, password }) => {
+            const user = await User.findOne({ email });
+            if(!user) {
+                throw new AuthenticationError('No user found');
+            }
+        },
+        addUser: {
 
-    }
+        },
+        saveBook: {
+
+        },
+        removeBook: {
+
+        }
+    },
+
 };
 
 module.exports = resolvers;
