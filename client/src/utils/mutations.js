4ad8fb9b30d61ffc8abda39d387e66ac1eb5ee27
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-21 22:13:51 -0800

    defined mutation exports for LOGIN_USER, ADD_USER, & SAVE_BOOK

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,48 @@
-import { gql } from '@appolo/client'
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: STring!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($email: String!, $password: String!, username: String!) {
+        addUser(email: $email, password: $password, username: $username) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: customBook!) {
+        saveBook(book: $book) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+
+`;
