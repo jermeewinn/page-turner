f5be6e6aa1157bb659102295b9f6520f06161257
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-21 22:54:06 -0800

    Imported associated libraries from @apollo/client node package

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// Apollo client imports
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
+
 import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '';
+    },
+  },
+});
 
 function App() {
   return (
