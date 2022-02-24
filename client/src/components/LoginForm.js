1c7357990638f5da6ba39af6a5efdab88758d5db
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-23 19:10:50 -0800

    added changes to the file to help with implementing Login function using GraphQL API

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,31 +1,29 @@
 // see SignupForm.js for comments
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
-
-// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
-import { loginUser } from '../utils/API';
+
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [ loginUser, { error }] = useMutation(LOGIN_USER);
+  const [ loginUser ] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
-  useEffect(() => {
-    if (error) { 
-      setShowAlert(true)
-    } else {
-      setShowAlert(false)
-    }
-  }, [error]);
+  // useEffect(() => {
+  //   if (error) { 
+  //     setShowAlert(true)
+  //   } else {
+  //     setShowAlert(false)
+  //   }
+  // }, [error]);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -38,11 +36,10 @@ const LoginForm = () => {
     }
 
     try {
-      //create formatting similar to SignUpForm.js
       const { data } = await loginUser({ variables: {
         ...userFormData 
       }});
-      Auth.login(data.loginUser.token)
+      Auth.login(data.loginUser.token);
 
       // if (!response.ok) {
       //   throw new Error('something went wrong!');
