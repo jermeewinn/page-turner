1ef109f504e75b6178ec99db9e0d523c301d2f19
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-23 19:23:52 -0800

    cleaned up code for SignupForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
-
-// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -41,15 +39,6 @@ const SignupForm = () => {
       });
 
       Auth.login(data.addUser.token);
-    
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
-      // const { token, user } = await response.json();
-      // console.log(user);
-      // Auth.login(token);
     } catch (e) {
       console.error(e);
       setShowAlert(true);
